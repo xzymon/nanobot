b86c3277c21ae6b03885d012c546bc79355f47bc
xzymon <xzymon@gmail.com>
2025-05-26 15:49:40 +0200

    Repo Init

---
+++ src/main/java/com/xzymon/nanobot/NanobotApplication.java
@@ -0,0 +1,13 @@
+package com.xzymon.nanobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xzymon/nanobot/NanobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzymon.nanobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
